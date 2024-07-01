61dd6ffadad83bbee66467558f1fcd3bdf88247b
Nuno Lopes <nuno.lopes@tecnico.ulisboa.pt>
2024-07-01 19:00:22 +0100

    add a few tests from @regehr

--- tools/transform.cpp
+++ tools/transform.cpp
@@ -1251,6 +1251,10 @@ static void calculateAndInitConstants(Transform &t) {
   bits_for_offset = min(bits_for_offset, config::max_offset_bits);
   bits_for_offset = min(bits_for_offset, bits_program_pointer);
 
+  // we may have an implicit ptr2int through memory. Ensure we have enough bits
+  if (has_ptr_load && does_int_mem_access && t.tgt.has(FnAttrs::Asm))
+    bits_for_offset = bits_program_pointer;
+
   // ASSUMPTION: programs can only allocate up to half of address space
   // so the first bit of size is always zero.
   // We need this assumption to support negative offsets.
@@ -1272,7 +1276,7 @@ static void calculateAndInitConstants(Transform &t) {
                              bits_ptr_address) + has_local_bit,
                          bits_program_pointer);
 
-  if (config::tgt_is_asm)
+  if (t.tgt.has(FnAttrs::Asm))
     bits_ptr_address = bits_program_pointer;
 
   // TODO: this is only needed if some program pointer is observable
